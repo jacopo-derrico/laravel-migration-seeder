38d1528cfa35511c3f5400c63103d02d5a2be78f
jacopo-derrico <derricoj.design@gmail.com>
2024-03-16 12:16:06 +0100

    workspace setup

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    @vite('resources/js/app.js')
+
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-center">hello world</h1>
+</body>
+</html>
